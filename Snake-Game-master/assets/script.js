36fc9c854984a9f93689b41368e8038023f48532
Ashok Kumar saini <askumar9672929010@gmail.com>
2021-05-18 20:56:01 +0530

    Update script.js

--- Snake-Game-master/assets/script.js
+++ Snake-Game-master/assets/script.js
@@ -11,7 +11,7 @@ function setup(){
 }
 
 function pickLocation() {
-  var cols = floor(width/scl);
+  var cols = floor(width/scl); //the canvas area is made into grid by this function so that our our food can be superimposed on snake ..
   var rows = floor(height/scl);
   food = createVector(floor(random(cols)), floor(random(rows)));
   food.mult(scl);
@@ -23,7 +23,7 @@ function draw() {
   s.death();
   s.update();
   s.show();
-  
+  //if snake eat the food then new location for food
   if (s.eat(food)) {
    pickLocation();
   }
@@ -33,7 +33,7 @@ function draw() {
   rect(food.x, food.y, scl, scl);
 }
 
-function keyPressed() {
+function keyPressed() {// various effects on key press
   if (keyCode === UP_ARROW) {
     s.dir(0, -1);
   } else if (keyCode === DOWN_ARROW){
@@ -45,7 +45,7 @@ function keyPressed() {
   }
 }
 
-function Snake() {
+function Snake() { //
   this.x = 0;
   this.y = 0;
   this.xspeed = 1;
@@ -73,19 +73,20 @@ function Snake() {
     for (var i = 0; i < this.tail.length; i++) {
       var pos = this.tail[i];
       var d = dist(this.x, this.y, pos.x, pos.y);
-      if (d < 1) {
+      if (d < 1) {    //id distance between tail and current is less than 1 then start fresh the snake loses its length
         this.total = 0;
         this.tail = [];
       }
     }
   }
   
-  this.update = function () {
-    for (var i = 0; i < this.tail.length-1; i++) {
+  this.update = function () { // To update the length of our sanke
+    for (var i = 0; i < this.tail.length-1; i++) { // the position of one block of snake body is the position that the next block  has currently
       this.tail[i] = this.tail[i+1];
     }
-    this.tail[this.total-1] = createVector(this.x, this.y);
+    this.tail[this.total-1] = createVector(this.x, this.y); // adds a new block to our snake
     
+    //updating our position of sanke current head(this)
     this.x = this.x + this.xspeed*scl;
     this.y = this.y + this.yspeed*scl;
     //avoid snake to go out of canvas
@@ -95,7 +96,7 @@ function Snake() {
   
   this.show = function() {
     fill(1,254,0);
-    for (var i = 0; i < this.total; i++){
+    for (var i = 0; i < this.total; i++){  // giving the block a rectangle form 
      rect(this.tail[i].x,this.tail[i].y, scl, scl);
     } 
     rect(this.x, this.y, scl, scl);
